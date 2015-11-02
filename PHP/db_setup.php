4f917b1caea843cc4850e50160cf0ac8ac7a3d9c
Addono <a.d.knapen@student.tue.nl>
2015-11-02 12:57:09 +0100

    Added till balance and roles to users table and auto creation of till user.

--- PHP/db_setup.php
+++ PHP/db_setup.php
@@ -4,6 +4,10 @@ defined( 'ABSPATH' ) or die( 'Not Even Close, Baby' );
 
 global $wpdb;
 
+/*
+ * @Description: Create necessary tables
+ */
+
 $user_table_name = $wpdb->prefix . 'mb_users';
 $transactions_table_name = $wpdb->prefix . 'mb_transactions';
 	
@@ -16,16 +20,19 @@ $sql = "
         firstname tinytext NOT NULL,
         lastname tinytext,
         balance float(10,2) DEFAULT '0' NOT NULL,
+        till float(10,2) DEFAULT '0' NOT NULL,
+        role ENUM('till manager', 'user', 'till') DEFAULT 'user' NOT NULL,
         UNIQUE KEY id (id)
     ) $charset_collate;
     
     CREATE TABLE $transactions_table_name (
         cdate datetime DEFAULT NOW() NOT NULL,
+        sourceid mediumint NOT NULL,
         targetid mediumint NOT NULL,
         authorid mediumint NOT NULL,
         amount float(5,2) NOT NULL,
         description tinytext,
-        type ENUM('purchase', 'decleration', 'payout', , 'refund', 'unknown', 'error') DEFAULT 'error' NOT NULL,
+        type ENUM('purchase', 'decleration', 'payout', 'refund', 'unknown', 'error') DEFAULT 'error' NOT NULL,
         state ENUM('new', 'unapproved', 'confirmed', 'finished', 'error', 'canceled') DEFAULT 'error' NOT NULL,
         method ENUM('internal', 'cash', 'deposit')
     ) $charset_collate;
@@ -33,4 +40,19 @@ $sql = "
 
 require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 dbDelta( $sql );
+
+/*
+ * @Description: Create 'till user' data if it doesn't exist yet.
+ */
+$result = $wpdb->get_results("SELECT * FROM $user_table_name WHERE id = '0';");
+
+if(count($result) == 0) {
+    $till_data = [
+        id => 0,
+        role => 'till',
+        firstname => 'till'
+    ];
+    
+    $wpdb->insert($user_table_name, $till_data);
+}
 ?>
