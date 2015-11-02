cfed46cec1c28f05070acbb77979822c455de10a
Addono <a.d.knapen@student.tue.nl>
2015-11-02 12:11:53 +0100

    Added transactions table

--- PHP/db_setup.php
+++ PHP/db_setup.php
@@ -4,18 +4,31 @@ defined( 'ABSPATH' ) or die( 'Not Even Close, Baby' );
 
 global $wpdb;
 
-$table_name = $wpdb->prefix . 'mb_users';
+$user_table_name = $wpdb->prefix . 'mb_users';
+$transactions_table_name = $wpdb->prefix . 'mb_transactions';
 	
 $charset_collate = $wpdb->get_charset_collate();
 
-$sql = "CREATE TABLE $table_name (
-    id mediumint NOT NULL AUTO_INCREMENT,
-    creationtime datetime DEFAULT NOW() NOT NULL,
-    firstname tinytext NOT NULL,
-    lastname tinytext,
-    balance float(10,2) DEFAULT '0' NOT NULL,
-    UNIQUE KEY id (id)
-    ) $charset_collate;";
+$sql = "
+    CREATE TABLE $user_table_name (
+        id mediumint NOT NULL,
+        cdate datetime DEFAULT NOW() NOT NULL,
+        firstname tinytext NOT NULL,
+        lastname tinytext,
+        balance float(10,2) DEFAULT '0' NOT NULL,
+        UNIQUE KEY id (id)
+    ) $charset_collate;
+    
+    CREATE TABLE $transactions_table_name (
+        cdate datetime DEFAULT NOW() NOT NULL,
+        targetid mediumint NOT NULL,
+        authorid mediumint NOT NULL,
+        amount float(5,2) NOT NULL,
+        description tinytext,
+        type ENUM('purchase', 'decleration', 'upgrade', 'refund', 'unknown', 'error') DEFAULT 'error' NOT NULL,
+        state ENUM('new', 'unapproved', 'confirmed', 'finished', 'error', 'canceled') DEFAULT 'error' NOT NULL
+    ) $charset_collate;
+    ";
 
 require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 dbDelta( $sql );
