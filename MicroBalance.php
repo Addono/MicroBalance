a41b415b8fc59fe9df8aca7ff678314c13cbd4ef
Addono <a.d.knapen@student.tue.nl>
2015-11-03 14:35:11 +0100

    Version incremention

--- MicroBalance.php
+++ MicroBalance.php
@@ -6,7 +6,6 @@ defined( 'ABSPATH' ) or die( 'Not Even Close, Baby!' );
  * Plugin Name: MicroBalance
  * Plugin URI: 
  * Description: 
- * Version: 0.0.3
  * Author: Adriaan Knapen <a.d.knapen@student.tue.nl>
  * Author URI: 
  * License: 
