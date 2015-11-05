fb23feed624c0779f07fef664983c3ce26106f40
Addono <a.d.knapen@student.tue.nl>
2015-11-05 13:37:18 +0100

    Added divs around the transactions and journal entries

--- PHP/transactions.php
+++ PHP/transactions.php
@@ -7,12 +7,19 @@ global $wpdb;
 $ids = explode(':', $_GET['id']);
 
 foreach($ids as $id) {
+    
+    echo "<div class='transaction'>\n";
     echo_transaction($id, "h1");
     
     $journals = $wpdb->get_results("SELECT * FROM " . get_table('journal') . " WHERE transactionid = '$id'");
     
     foreach($journals as $journal) {
+        echo "<div class='journal'>\n";
         echo_journal_entry($journal->journalid, 'h2');
+        echo "</div>\n";
     }
+    
+    
+    echo "</div>\n";
 }
 ?>
