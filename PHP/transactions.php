346dd7635f4ca1b6e19958c27ea74235fc398099
Addono <a.d.knapen@student.tue.nl>
2015-11-05 14:46:57 +0100

    If no transactions are specified, show the latest 20.

--- PHP/transactions.php
+++ PHP/transactions.php
@@ -6,6 +6,21 @@ global $wpdb;
 
 $ids = explode(':', $_GET['id']);
 
+if($ids[0] == "") {
+    $result = $wpdb->get_results("SELECT * FROM " . get_table('transactions') . " ORDER BY transactionid DESC LIMIT 20");
+    
+    if($result[0] == "") {
+        echo "<h2><i>" . __('No transactions found.', 'MicroBalance') . "</i></h2>\n";
+    } else {
+        echo "<p><i>" . sprintf(_n('Only one transaction found.', 'Showing the latest %s transactions.', 'MicroBalance'), count($result)) . "</i></p><br>\n";
+        
+        foreach($result as $id) {
+            $ids[] = $id->transactionid;
+        }
+    }
+    
+}
+
 foreach($ids as $id) {
     
     echo "<div class='transaction'>\n";
@@ -18,7 +33,6 @@ foreach($ids as $id) {
         echo_journal_entry($journal->journalid, 'h2');
         echo "</div>\n";
     }
-    
     
     echo "</div>\n";
 }
