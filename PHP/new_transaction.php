0b44d911d5862153c3f80ed11fe5150bcbb941fb
Addono <a.d.knapen@student.tue.nl>
2015-11-25 16:10:48 +0100

    Now shows new inventory purchase and new upgrade.

--- PHP/new_transaction.php
+++ PHP/new_transaction.php
@@ -3,10 +3,16 @@
 defined( 'ABSPATH' ) or die( 'Not Even Close, Baby!' );
 
 $messages = [];
-require('new_inventory_purchase.php');
-
-
+//echo new_payment(1, 2, 5, "Placeholder");
 ?>
+<div class="transaction_type">
+    <?php require('new_inventory_purchase.php'); ?>
+</div>
+
+<div class="transaction_type">
+    <?php require('new_upgrade.php'); ?>
+</div>
+
 <div style='float:left; margin-left:2em'>
 <?php
     foreach($messages as $message) {
