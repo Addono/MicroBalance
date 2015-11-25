72cb82757df292344c7f63d694ac33047a8ad00a
Addono <a.d.knapen@student.tue.nl>
2015-11-25 16:10:01 +0100

    Added new upgrade body.

--- PHP/new_upgrade.php
+++ PHP/new_upgrade.php
@@ -5,3 +5,30 @@ defined( 'ABSPATH' ) or die( 'Not Even Close, Baby!' );
 if($_POST['type'] == "upgrade") {
 
 }
+?>
+<h1><?php _e("Register upgrade", "MicroBalance"); ?></h1>
+    <form method="post">
+        <div>
+            <h2><?php _e('Payed by', 'MicroBalance'); ?></h2>
+                <?php get_user_selector(false, "payed_by"); ?>
+        </div>
+
+        <div style='float:left'>
+            <h2><?php _e('Payed to', 'MicroBalance'); ?></h2>
+                <?php get_user_selector(false, "payed_to"); ?>
+        </div>
+        
+        <div style='float:left; margin-left:2em'>
+            <h2><?php _e('Amount', 'MicroBalance'); ?></h2>
+            <input type='text' name='amount'><br>
+
+            <h2><?php _e('Description', 'MicroBalance'); ?></h2>
+            <textarea rows=10 name='description'><?php if(!$proceed) echo $_POST['description']; ?></textarea>
+        </div>
+    
+        <div style='float:left; margin-left:2em'>
+            <?php submit_button(__('Submit', 'MicroBalance')); ?>
+        </div>
+        <input type='hidden' name='type' value='inventory purchase'>
+    </form>
+        
